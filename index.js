8925da61261674bd3a810ddffa54d79726025cec
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-10-13 23:11:20 +0200

    set-up express server

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import { express } from "express"; // import express
+import express from "express";
 import bodyParser from "body-parser"; //  Allows to take in incoming post request bodies
 
 const app = express();  // initialise our express application.
 const PORT = 5000    // app port
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.listen(PORT, ()=> console.log (`Server running on port: http://localhost${PORT}`))
+
+
+
+
 
