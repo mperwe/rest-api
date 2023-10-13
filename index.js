744d87bc7c362b596194d49b1b2b02a4150c12ce
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-10-13 21:58:40 +0200

    initialise express app

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import { Express } from "express";
+import { express } from "express"; // import express
+import bodyParser from "body-parser"; //  Allows to take in incoming post request bodies
+
+const app = express();  // initialise our express application.
+const PORT = 5000    // app port
+
+app.use(bodyParser.json())
+
