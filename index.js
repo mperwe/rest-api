b2cb49e975414a6090fe5faaf2123af66f7f8901
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-10-14 00:11:24 +0200

    create route

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import express from "express";
 import bodyParser from "body-parser"; //  Allows to take in incoming post request bodies
 
 const app = express();  // initialise our express application.
-const PORT = 5000    // app port
+const PORT = 5000    // application port
 
 app.use(bodyParser.json());
 
+//create route
+app.get('/', (req , res)=>{
+    console.log('[TEST]!');
+    res.send('Hello from Homepage.');
+
+});
+
 app.listen(PORT, ()=> console.log (`Server running on port: http://localhost:${PORT}`))
+
+
 
 
 
