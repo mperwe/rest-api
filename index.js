8924d8e925d6b9bbd705b46367bf1565e2b47120
Frank Mperwe Kyambadde <frankmperwe@gmail.com>
2023-10-14 12:11:47 +0200

    creating users

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from "express";
-import bodyParser from "body-parser"; 
+import bodyParser from "body-parser"; //  Allows to take in incoming post request bodies
+
 import usersRoutes from './routes/users.js';
-const app = express();  
-const PORT = 5000    
+
+const app = express();  // initialise our express application.
+const PORT = 5000    // application port
+
 app.use(bodyParser.json());
+
 app.use('/users', usersRoutes); 
-app.get('/', (req, res) => res.send('Hello from Homepage.')); //create route
+
+//create route
+app.get('/', (req, res) => res.send('Hello from Homepage.'));
+
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
 
 
